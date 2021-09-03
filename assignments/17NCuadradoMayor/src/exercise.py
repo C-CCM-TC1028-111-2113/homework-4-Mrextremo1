2d0c29b314ffecbf85c0cdc46bea50892a4a592b
Leonardo <leonardomedrano23@hotmail.com>
2021-09-03 11:07:58 -0500

    done

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -2,7 +2,10 @@
 
 def main():
     #Escribe tu código debajo de esta línea
-    pass
-
+    n = int(input("Escribe un numero : "))
+    for i in range(n):
+        if i**2 > n:
+            break
+    print(i)
 if __name__=='__main__':
     main()
