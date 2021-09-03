2ab9f39d461bfc0fc8f54377b4f24b46f625d736
Leonardo <leonardomedrano23@hotmail.com>
2021-09-03 13:49:23 -0500

    done

--- assignments/28Fibonacci/src/exercise.py
+++ assignments/28Fibonacci/src/exercise.py
@@ -1,7 +1,29 @@
 
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    n = int(input())
+    i = 1
+    f1 = 0
+    f2 = 1
+    if n == 0:
+        x = f1
+    elif n == 1:
+        x = f2
+    while i <= n and n > 1: 
+        f1 = f1 + f2
+        i = i + 1
+        if i == n:
+            x = f1
+            break
+        f2 = f1 + f2
+        i = i + 1
+        if i == n:
+            x = f2
+            break
+    print(x)
+    
+
+        
 
 if __name__=='__main__':
     main()
