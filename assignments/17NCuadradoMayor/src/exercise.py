2b3a73d17b21fd574fe985577dc1e57fcb0c11e4
Leonardo <leonardomedrano23@hotmail.com>
2021-09-03 11:15:40 -0500

    done

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -3,7 +3,7 @@
 def main():
     #Escribe tu código debajo de esta línea
     n = int(input("Escribe un numero : "))
-    for i in range(n):
+    for i in range(0, n+2):
         if i**2 > n:
             break
     print(i)
