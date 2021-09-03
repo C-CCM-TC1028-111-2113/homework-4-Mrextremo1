aaad5ffd29fbf8b3b7874e99d49385120588076f
Leonardo <leonardomedrano23@hotmail.com>
2021-09-03 10:59:58 -0500

    Done

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,6 +1,6 @@
 def main():
     #escribe tu código abajo de esta línea
-    n = int(input())
+    n = int(input("ingresa un numero"))
     for i in range(1, n+1):
         if i % 2 == 0:
             print(str(i) + "-%")
